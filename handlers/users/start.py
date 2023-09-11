a42d8ccd1e54d949f65df293a43d91e8a04fafdd
Azamat Dosmukhambetov <you@example.com>
2023-09-11 23:58:07 +0500

    updated

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -18,7 +18,7 @@ async def bot_start(message: types.Message,state: FSMContext):
         # Adminga xabar beramiz
         count = db.count_users()[0]
         await bot.send_message(chat_id=ADMINS[0],text=f"Bazaga {name} qo'shildi\nBazada {count} ta foydalanuvchi bor")
-        await message.answer(f"Xush kelibsiz {name}")
+        await message.answer(f"Xush kelibsiz {name}!")
     except sqlite3.IntegrityError as err:
         await bot.send_message(chat_id=ADMINS[0],text=f"Bazaga {name} qo'shildi\nBazada {count} ta foydalanuvchi bor")
-        await message.answer(f"Xush kelibsiz {name}")
+        await message.answer(f"Xush kelibsiz {name}!")
